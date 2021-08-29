23c8aa50774821dd77ec51a078a05cfba96b1378
Ashwin P S <ashwinsiva54@gmail.com>
2021-08-29 19:19:14 +0530

    div: npm run build

--- src/components/Name.js
+++ src/components/Name.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Name = () => {
     return (
         <div className='myName'>
-            - By&nbsp;<a href='https://ashwin-p-s.github.io/portfolio' target="_blank" rel="noreferrer" className='myNameLink'>Ashwin P S</a>
+            - By&nbsp;<a href='https://ashwin-p-s.github.io/portfolio' target="_blank" rel="noreferrer" className='myNameLink'>Ashwin Ps</a>
         </div>
     )
 }
